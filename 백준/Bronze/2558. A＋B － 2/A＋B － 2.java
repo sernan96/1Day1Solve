0ff4332ba4991e874125bc6e2758dedde8d90d8d
Hwang gend <80672346+sernan96@users.noreply.github.com>
2024-01-02 23:38:48 +0900

    [Bronze V] Title: A+B - 2, Time: 120 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+public class Main {
+    static int min;
+    public static void main(String []args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        System.out.println(a+b);
+    }
+}
