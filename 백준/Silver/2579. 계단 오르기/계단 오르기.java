f8154b991c796159f939ec93ba43fab4dc4c7c71
Hwang gend <80672346+sernan96@users.noreply.github.com>
2024-02-04 16:54:12 +0900

    [Silver III] Title: 계단 오르기, Time: 120 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -3,28 +3,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static Integer []dp ;
+    static int []dp ;
     static int []score;
     public static void main(String []args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         score = new int[n+1];
-        for(int i=1; i<=n; i++){
+
+        for(int i=1; i<=n; i++)
             score[i]= Integer.parseInt(br.readLine());
-        }
-        dp = new Integer[n+1];
-        dp[0] = score[0];//Integer로 클래스 형식으로 배열을 선언해주어서 초기화를 안하면 NULL이다.
-        dp[1]= score[1];
-        if(n>1){
-            dp[2]= score[1]+score[2];
-        }
+
+        dp = new int[n+1];
+
         System.out.print(find(n));
     }
     static int find(int n){
-        if(dp[n]==null){
+        if(n < 1) return 0;
+        if(dp[n]==0)
             dp[n] = Math.max(find(n-2), find(n-3)+score[n-1])+score[n];
-        }
         return dp[n];
     }
-
 }
