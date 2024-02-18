30ec34c04d645a7b118df8b2fb421e20222522da
Hwang gend <80672346+sernan96@users.noreply.github.com>
2024-02-18 21:26:41 +0900

    [Silver II] Title: 나무 자르기, Time: 532 ms, Memory: 119216 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,47 +1,45 @@
-    import java.io.BufferedReader;
-    import java.io.IOException;
-    import java.io.InputStreamReader;
-    import java.util.StringTokenizer;
+import java.io.*;
+import java.util.StringTokenizer;
 
-    public class Main {
-        static int n, m, l, min=0, max= -1;
-        public static void main(String []args) throws IOException{
-            int [] arr= input();
-            binary_search(arr);
-        }
+public class Main {
+    static int n, m, min = 0, max = -1;
 
-        private static void binary_search(int [] arr) {
-            while(min < max) {
-                int mid = (min + max) / 2;
-                long sum = 0;
-                for(int tree_length : arr) {
-                    if(tree_length - mid > 0) {
-                        sum += (tree_length - mid);
-                    }
-                }
-                if(sum < m) {
-                    max = mid;
-                }
-                else {
-                    min = mid + 1;
-                }
-            }
-            System.out.println(min-1);
-        }
+    public static void main(String[] args) throws IOException {
+        input();
 
-        private static int [] input() throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringBuilder result  = new StringBuilder();
-            StringTokenizer nm = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(nm.nextToken());
-            m = Integer.parseInt(nm.nextToken());
-            int []arr = new int[n];
-            StringTokenizer tree_length = new StringTokenizer(br.readLine());
-            for(int i =0; i<n; i++){
-                l = Integer.parseInt(tree_length.nextToken());
-                arr[i] = l;
-                max = Math.max(max, l);
-            }
-            return arr;
-        }
     }
+
+    private static void binary_search(int[] arr) {
+        while (min < max) {
+            int mid = (min + max) / 2;
+            long sum = 0;
+            for (int tree_length : arr) {
+                if (tree_length - mid > 0) {
+                    sum += (tree_length - mid);
+                }
+            }
+            if (sum < m) {
+                max = mid;
+            } else {
+                min = mid + 1;
+            }
+        }
+        System.out.println(min - 1);
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder result = new StringBuilder();
+        StringTokenizer nm = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(nm.nextToken());
+        m = Integer.parseInt(nm.nextToken());
+        int[] arr = new int[n];
+        StringTokenizer tree_length = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int l = Integer.parseInt(tree_length.nextToken());
+            arr[i] = l;
+            max = Math.max(max, l);
+        }
+        binary_search(arr);
+    }
+}
