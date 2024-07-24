9688b0598483a542326e6a654f08238be6038ff3
Hwang gend <80672346+sernan96@users.noreply.github.com>
2024-07-24 14:17:27 +0900

    [Gold IV] Title: 알파벳, Time: 796 ms, Memory: 17020 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -2,35 +2,39 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    static  char [][]board;
-    static int max=-1, R, C;
+    static char[][] board;
+    static int max = -1, rows, cols;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        board = new char[R+2][C+2];
-        for(int i =1; i<R+1; i++){
+        rows = Integer.parseInt(st.nextToken());
+        cols = Integer.parseInt(st.nextToken());
+        board = new char[rows + 2][cols + 2];
+        
+        for (int i = 1; i <= rows; i++) {
             String input = br.readLine();
-            for(int j=1; j<C+1; j++){
-                board[i][j]=input.charAt(j-1);
+            for (int j = 1; j <= cols; j++) {
+                board[i][j] = input.charAt(j - 1);
             }
         }
-        boolean [] visited = new boolean['Z'+1];
+        
+        boolean[] visited = new boolean[26]; // 'A' to 'Z'
         move(1, 1, visited, 0);
         System.out.print(max);
     }
-    static void move (int x, int y, boolean []visited, int cnt){
-        if(visited[board[x][y]]||(x<1||y<1||x>R||y>C)){
 
-            max = Math.max(max, cnt);
+    static void move(int x, int y, boolean[] visited, int count) {
+        if (x < 1 || y < 1 || x > rows || y > cols || visited[board[x][y] - 'A']) {
+            max = Math.max(max, count);
             return;
         }
-        visited[board[x][y]]=true;
-        move(x-1, y, Arrays.copyOf(visited, visited.length), cnt+1);
-        move(x+1, y, Arrays.copyOf(visited, visited.length), cnt+1);
-        move(x, y-1, Arrays.copyOf(visited, visited.length), cnt+1);
-        move(x, y+1, Arrays.copyOf(visited, visited.length), cnt+1);
+        
+        visited[board[x][y] - 'A'] = true;
+        move(x - 1, y, visited, count + 1);
+        move(x + 1, y, visited, count + 1);
+        move(x, y - 1, visited, count + 1);
+        move(x, y + 1, visited, count + 1);
+        visited[board[x][y] - 'A'] = false; // Backtrack
     }
-
 }
