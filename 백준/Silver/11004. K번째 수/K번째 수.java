c8d20a3846373fb7a57ca87e38b994a8e517aaaa
Hwang gend <80672346+sernan96@users.noreply.github.com>
2024-11-13 01:38:38 +0900

    [Silver V] Title: K번째 수, Time: 2264 ms, Memory: 382408 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int [] arr = new int[n];
+        StringTokenizer num = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(num.nextToken());
+        }
+        Arrays.sort(arr);
+        System.out.print(arr[k-1]);
+    }
+}
