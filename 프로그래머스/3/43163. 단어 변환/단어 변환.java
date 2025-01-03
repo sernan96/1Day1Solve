1802f85509fefbdebf744854d538243dd5fe0150
Hwang gend <80672346+sernan96@users.noreply.github.com>
2025-01-03 12:00:40 +0900

    [level 3] Title: 단어 변환, Time: 0.45 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,32 +1,32 @@
 import java.util.*;
-
 class Solution {
     static int result;
+    static ArrayList<String> arr;
     public int solution(String begin, String target, String[] words) {
         result = Integer.MAX_VALUE;
-        ArrayList<String> arr = new ArrayList<>(Arrays.asList(words));
+        arr = new ArrayList<>(Arrays.asList(words));
         if (!arr.contains(target)) return 0; // target이 없으면 0 반환
         boolean[] visited = new boolean[arr.size()];
-        findMin(begin, target, arr, visited, 0);
+        findMin(begin, target, visited, 0);
         return result == Integer.MAX_VALUE ? 0 : result;
     }
 
-    static void findMin(String now, String target, ArrayList<String> arr, boolean[] visited, int cnt) {
+    static void findMin(String now, String target, boolean[] visited, int cnt) {
         if (now.equals(target)) {
             result = Math.min(result, cnt);
             return;
         }
 
         for (int i = 0; i < arr.size(); i++) {
-            if (!visited[i] && isConvertible(now, arr.get(i))) {
+            if (!visited[i] && check(now, arr.get(i))) {
                 visited[i] = true;
-                findMin(arr.get(i), target, arr, visited, cnt + 1);
+                findMin(arr.get(i), target, visited, cnt + 1);
                 visited[i] = false;
             }
         }
     }
 
-    static boolean isConvertible(String a, String b) {
+    static boolean check(String a, String b) {
         int diff = 0;
         for (int i = 0; i < a.length(); i++) {
             if (a.charAt(i) != b.charAt(i)) diff++;
