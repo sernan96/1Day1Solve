377c378c206343403daa675cf329e2fa6261cc0d
Hwang gend <80672346+sernan96@users.noreply.github.com>
2025-02-15 00:37:10 +0900

    [Silver I] Title: 동물원, Time: 116 ms, Memory: 18528 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[][] dp = new long[N + 1][3];
+        dp[1][0] = dp[1][1] = dp[1][2] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % 9901;
+        }
+
+        System.out.print((dp[N][0] + dp[N][1] + dp[N][2]) % 9901);
+
+    }
+}
