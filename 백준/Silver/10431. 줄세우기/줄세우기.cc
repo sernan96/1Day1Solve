1adbec3b9ad63daa8b2e7cc7f512e14181ecceeb
Hwang gend <80672346+sernan96@users.noreply.github.com>
2025-12-26 00:04:51 +0900

    [Silver V] Title: 줄세우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.cc
+++ 백준/Silver/10431. 줄세우기/줄세우기.cc
@@ -6,39 +6,30 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-
     int T;
     cin >> T;
-    while (T--) {
+    while (T--!=0) {
         int t;
         cin >> t;
         int cnt = 0;
         vector<int> students(20, 0);
-
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 20;i++) {
             int student;
             cin >> student;
-            
-            // 일단 현재 위치에 넣는다고 가정 (임시 저장 필요 없음, 밀어내고 넣으면 됨)
-            // students[i] = student; // 이 줄은 아래 로직에서 처리하므로 생략 가능하나 있어도 무방
-
-            // 앞에 자신보다 키 큰 학생이 있는지 탐색
-            bool inserted = false;
+            bool isIt = false;
+            //앞에 새 학생보다 키큰 경우가 있는지
             for (int j = 0; j < i; j++) {
-                if (students[j] > student) {
-                    // *** 수정된 부분: 뒤에서부터 앞으로 밀어야 함 ***
-                    for (int k = i - 1; k >= j; k--) {
+                if (students[j]>student) {
+                    isIt = true;
+                    for (int k = i-1; k >=j; k--) {
                         students[k + 1] = students[k];
-                        cnt++; // 한 칸 밀릴 때마다 걸음 수 증가
+                        cnt++;
                     }
-                    students[j] = student; // 빈 자리에 새 학생 넣기
-                    inserted = true;
+                    students[j] = student;
                     break;
                 }
             }
-            
-            // 만약 앞에 더 큰 학생이 없어서 중간에 끼지 않았다면, 맨 뒤(i)에 넣음
-            if (!inserted) {
+            if (!isIt) {
                 students[i] = student;
             }
         }
@@ -47,3 +38,8 @@ int main() {
 
     return 0;
 }
+/*
+새로운 학생이 추가될때 새로운 학생보다 키가 큰 학생의 수를 세어 + 해주면 된다.
+그럼 학생이 중간에 낄때 뒤로 미뤄주는 것은 어떻게?
+
+*/
