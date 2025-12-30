59b32825b556b9ccc4b450f366590245d1dab353
Hwang gend <80672346+sernan96@users.noreply.github.com>
2025-12-30 16:46:08 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2260 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -3,32 +3,29 @@
 #include<algorithm>
 using namespace std;
 
-long long dp[10001][2] = { {0,}, };
+int dp[10001][2] = { {0,}, };
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 	int N;
 	cin >> N;
-	
 	vector<int> wines(N + 1, 0);
+
 	for (int i = 1; i <= N; i++) {
 		cin >> wines[i];
 	}
+	//3 이하인 경우는 따로 처리
 	if (N < 3) {
-		if (N == 1) {
-			cout << wines[1];
-			return 0;
-		}
-		else {
-			cout << wines[1] + wines[2];
-			return 0;
-		}
+		int result = N == 1 ? wines[1] : wines[1] + wines[2];
+		cout << result;
+		return 0;
 	}
-	dp[1][0] = dp[1][1] = wines[1];
+	// 초기값 세팅
+	dp[1][0] = dp[1][1] = wines[1]; // 이전 값이 없기때문에 i=3에서 i=1인 dp값을 불러올 상황 대비
 	dp[2][0] = wines[2];
 	dp[2][1] = dp[1][0] + wines[2];
-	//dp [i][c] = max(dp[i-1][c+1] + wines[i], dp[i-2][0,1,2]) c<2;
+
 	for (int i = 3; i <= N; i++) {
 		//연속하지 않은 경우에 대해서 최신화
 		dp[i][0] = max(max(dp[i - 2][0], dp[i - 2][1]) + wines[i], max(dp[i - 1][0], dp[i-1][1]));
@@ -38,9 +35,3 @@ int main() {
 	cout << max(dp[N][0], dp[N][1]);
 	return 0;
 }
-/*
-연속 3번은 불가능 하다
-그렇다면 N+1*3크기의 dp 배열을 통해
-연속으로 먹으면 [i][2]
-5분
-*/
